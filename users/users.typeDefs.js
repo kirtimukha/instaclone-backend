9483ddf4917aeda907a18ddc07289dbb445b68aa
kirti <kirtimukha@nate.com>
2022-06-27 22:49:10 +0900

    [U] [6-27] 6-7 완료

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -2,7 +2,7 @@ import { gql } from "apollo-server"
 
 export default gql`
     type User{
-        id        : String!
+        id        : Int!
         firstName : String!
         lastName  : String
         username  : String!
@@ -14,9 +14,9 @@ export default gql`
         following: [User]
         followers: [User]
         totalFollowing: Int!
-        totalFollowers: Int!        
-        isMe: Boolean!  
-        isFollowing: Boolean! 
+        totalFollowers: Int!
+        isMe: Boolean!
+        isFollowing: Boolean!
     }
 `;
 //totalFollowing: Int! /* request에 의해서 생성되는 내용. login 여부와 무관함 */
